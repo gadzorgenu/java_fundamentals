5426ae216f5ba569183a1352e5ed482166490e5b
Georgina Adzorgenu <42786156+gadzorgenu@users.noreply.github.com>
2021-11-25 09:57:42 +0000

    Add files via upload

---
+++ View.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class View {
+
+    public void acceptResult(Task task, int value){
+     println("Task " + task.getNumber() + " finished result = "+ value);
+
+    }
+
+    public void displayResult(){}
+
+    public static void println(String s){
+        System.out.println(s);
+    }
+
+}
